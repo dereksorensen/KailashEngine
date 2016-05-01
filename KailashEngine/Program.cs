68b542275764b2bdb9c420eac7215a3ba7da2efa
jarvisjimson <der.sorensen@gmail.com>
2016-05-01 13:55:48 -0700

    Setup basic game window

--- KailashEngine/Program.cs
+++ KailashEngine/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
 namespace KailashEngine
 {
-    class Program
+    class Engine : GameWindow
     {
+
+
+        public Engine() :
+            base(800,600)
+        {
+
+        }
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
+            using (Engine KailashEngine = new Engine())
+            {
+                KailashEngine.Run(60.0, 0.0);
+            }
         }
     }
 }
