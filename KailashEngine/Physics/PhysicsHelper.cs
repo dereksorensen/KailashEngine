27fd16670fe237c863a3ac2510dff4cd2f7366c8
jarvisjimson <der.sorensen@gmail.com>
2016-10-11 19:42:16 -0700

    Added convex hull and mesh physics loading

--- KailashEngine/Physics/PhysicsHelper.cs
+++ KailashEngine/Physics/PhysicsHelper.cs
@@ -5,27 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 using BulletSharp;
-using OpenTK;
+using BulletSharp.Math;
 
 namespace KailashEngine.Physics
 {
     static class PhysicsHelper
     {
 
-        public static RigidBody CreateLocalRigidBody(PhysicsWorld physics_world, bool dynamic, float mass, float restitution, Matrix4 startTransform, CollisionShape shape)
+        public static RigidBody CreateLocalRigidBody(PhysicsWorld physics_world, bool dynamic, float mass, float restitution, Matrix startTransform, CollisionShape shape)
         {
             return CreateLocalRigidBody(physics_world, dynamic, mass, restitution, startTransform, shape, new Vector3(1.0f));
         }
 
 
-        public static RigidBody CreateLocalRigidBody(PhysicsWorld physics_world, bool dynamic, float mass, float restitution, Matrix4 startTransform, CollisionShape shape, Vector3 dimensions)
+        public static RigidBody CreateLocalRigidBody(PhysicsWorld physics_world, bool dynamic, float mass, float restitution, Matrix startTransform, CollisionShape shape, Vector3 dimensions)
         {
 
 
             //float scaler = dimensions.Length * 100.0f;
             //Console.WriteLine(scaler * mass);
 
-            BulletSharp.Math.Vector3 localInertia = EngineHelper.otk2bullet(Vector3.Zero);
+            BulletSharp.Math.Vector3 localInertia = Vector3.Zero;
             if (dynamic)
             {
                 shape.CalculateLocalInertia(mass, out localInertia);
@@ -35,7 +35,7 @@ namespace KailashEngine.Physics
                 mass = 0.0f;
             }
 
-            DefaultMotionState myMotionState = new DefaultMotionState(EngineHelper.otk2bullet(startTransform));
+            DefaultMotionState myMotionState = new DefaultMotionState(startTransform);
 
             RigidBodyConstructionInfo rbInfo = new RigidBodyConstructionInfo(mass, myMotionState, shape, localInertia);
             rbInfo.Restitution = restitution;
--- KailashEngine/Physics/PhysicsLoader.cs
+++ KailashEngine/Physics/PhysicsLoader.cs
@@ -6,15 +6,85 @@ using System.Text;
 using System.Threading.Tasks;
 
 using BulletSharp;
-using OpenTK;
+using BulletSharp.Math;
 
-using KailashEngine.Physics;
+
 using KailashEngine.World.Model;
 
 namespace KailashEngine.Physics
 {
     static class PhysicsLoader
     {
+
+        private static CollisionShape loadConvexHull(Mesh mesh, Vector3 scale)
+        {
+
+            TriangleMesh trimesh = new TriangleMesh();
+
+            // Shift vertices in so margin is not visible
+            Vector3 shift = Vector3.Normalize(scale) - new Vector3(0.04f);
+            for (int i = 0; i < mesh.submeshes[0].vertex_data.Length; i += 3)
+            {
+                int index0 = (int)mesh.submeshes[0].index_data[i];
+                int index1 = (int)mesh.submeshes[0].index_data[i + 1];
+                int index2 = (int)mesh.submeshes[0].index_data[i + 2];
+
+                Vector3 vertex0 = new Vector3(mesh.submeshes[0].vertex_data[index0].position.X, mesh.submeshes[0].vertex_data[index0].position.Z, -mesh.submeshes[0].vertex_data[index0].position.Y) * (scale);
+                //vertex0 *= shift;
+                Vector3 vertex1 = new Vector3(mesh.submeshes[0].vertex_data[index1].position.X, mesh.submeshes[0].vertex_data[index1].position.Z, -mesh.submeshes[0].vertex_data[index1].position.Y) * (scale);
+                //vertex1 *= shift;
+                Vector3 vertex2 = new Vector3(mesh.submeshes[0].vertex_data[index2].position.X, mesh.submeshes[0].vertex_data[index2].position.Z, -mesh.submeshes[0].vertex_data[index2].position.Y) * (scale);
+                //vertex2 *= shift;
+
+                trimesh.AddTriangle(vertex0, vertex1, vertex2);
+            }
+            ConvexShape tmpShape = new ConvexTriangleMeshShape(trimesh);
+
+            ShapeHull hull = new ShapeHull(tmpShape);
+            float margin = tmpShape.Margin;
+
+            hull.BuildHull(margin);
+            tmpShape.UserObject = hull;
+
+            ConvexHullShape convexShape = new ConvexHullShape();
+
+            foreach (Vector3 v in hull.Vertices)
+            {
+                convexShape.AddPoint(v);
+            }
+
+            hull.Dispose();
+            tmpShape.Dispose();
+
+
+            return convexShape;
+        }
+
+        private static CollisionShape loadConcaveMesh(Mesh mesh, Vector3 scale)
+        {
+            TriangleMesh trimesh = new TriangleMesh();
+
+            for (int i = 0; i < mesh.submeshes[0].vertex_data.Length; i += 3)
+            {
+                int index0 = (int)mesh.submeshes[0].index_data[i];
+                int index1 = (int)mesh.submeshes[0].index_data[i + 1];
+                int index2 = (int)mesh.submeshes[0].index_data[i + 2];
+
+                Vector3 vertex0 = new Vector3(mesh.submeshes[0].vertex_data[index0].position.X, mesh.submeshes[0].vertex_data[index0].position.Z, -mesh.submeshes[0].vertex_data[index0].position.Y) * (scale);
+                Vector3 vertex1 = new Vector3(mesh.submeshes[0].vertex_data[index1].position.X, mesh.submeshes[0].vertex_data[index1].position.Z, -mesh.submeshes[0].vertex_data[index1].position.Y) * (scale);
+                Vector3 vertex2 = new Vector3(mesh.submeshes[0].vertex_data[index2].position.X, mesh.submeshes[0].vertex_data[index2].position.Z, -mesh.submeshes[0].vertex_data[index2].position.Y) * (scale);
+
+                trimesh.AddTriangle(vertex0, vertex1, vertex2);
+            }
+
+            CollisionShape concaveShape = new BvhTriangleMeshShape(trimesh, true);
+
+            return concaveShape;
+        }
+
+
+
+
 
         public static void load(string filename, PhysicsWorld physics_world, Dictionary<string, UniqueMesh> meshes)
         {
@@ -69,9 +139,9 @@ namespace KailashEngine.Physics
                             break;
                         case "rot ":
                             Vector3 rot;
-                            rot.X = MathHelper.RadiansToDegrees(float.Parse(multi_value[0]));
-                            rot.Y = MathHelper.RadiansToDegrees(float.Parse(multi_value[1]));
-                            rot.Z = MathHelper.RadiansToDegrees(float.Parse(multi_value[2]));
+                            rot.X = OpenTK.MathHelper.RadiansToDegrees(float.Parse(multi_value[0]));
+                            rot.Y = OpenTK.MathHelper.RadiansToDegrees(float.Parse(multi_value[1]));
+                            rot.Z = OpenTK.MathHelper.RadiansToDegrees(float.Parse(multi_value[2]));
                             rotations.Add(rot);
                             break;
                         case "dim ":
@@ -116,78 +186,67 @@ namespace KailashEngine.Physics
                 string mesh_id = mesh_ids[i].Replace('.', '_');
                 string physics_id = ids[i].Replace('.', '_');
 
-                Vector3 position = positions[i];
-                Vector3 rotation = rotations[i];
-                Vector3 scale = scales[i];
-                Vector3 dimension = dimensions[i] / 2.0f;
-                bool dynamic = dynamics[i];
-                Vector3 attribute = attributes[i];
-                float mass = attribute.X;
-                float friction = attribute.Y;
-                float restitution = attribute.Z;
-
-
-
-                // Build transformation matrix
-                Matrix4 temp_matrix = EngineHelper.blender2Kailash(EngineHelper.createMatrix(position, rotation, new Vector3(1.0f)));
-
-
-                CollisionShape shape;
-                string collision_shape = shapes[i];
-                float radius;
-                switch (collision_shape)
-                {
-                    case "BOX":
-                        shape = new BoxShape(EngineHelper.otk2bullet(dimension));
-                        break;
-                    case "SPHERE":
-                        radius = (dimension.X + dimension.Y + dimension.Z) / 3.0f;
-                        shape = new SphereShape(radius);
-                        break;
-                    case "CONE":
-                        radius = (dimension.X + dimension.Z) / 4.0f;
-                        shape = new ConeShape(radius, dimension.Y);
-                        break;
-                    case "CYLINDER":
-                        shape = new CylinderShape(EngineHelper.otk2bullet(dimension));
-                        break;
-                    //case "CONVEX_HULL":
-                    //    if (meshD.TryGetValue(mesh_id, out temp_mesh))
-                    //    {
-                    //        shp = loadConvexHull(temp_mesh, scaler);
-                    //    }
-                    //    else
-                    //    {
-                    //        Console.WriteLine(cShape + " FAILED");
-                    //        continue;
-                    //    }
-                    //    break;
-                    //case "MESH":
-                    //    if (meshD.TryGetValue(mesh_id, out temp_mesh))
-                    //    {
-                    //        shp = loadConcaveMesh(temp_mesh, scaler);
-                    //    }
-                    //    else
-                    //    {
-                    //        Console.WriteLine(cShape + " FAILED");
-                    //        continue;
-                    //    }
-                    //    break;
-                    default:
-                        //numBodies--;
-                        Debug.DebugHelper.logError("\t" + collision_shape + " is not a supported collision shape", mesh_id);
-                        continue;
-                }
-
-
-                RigidBody collision_object = PhysicsHelper.CreateLocalRigidBody(physics_world, dynamic, mass, restitution, temp_matrix, shape, dimension);
-                collision_object.UserObject = physics_id;
-
-
-
                 UniqueMesh temp_unique_mesh;
                 if (meshes.TryGetValue(physics_id, out temp_unique_mesh))
                 {
+                    Vector3 position = positions[i];
+                    Vector3 rotation = rotations[i];
+                    Vector3 scale = scales[i];
+                    Vector3 dimension = dimensions[i] / 2.0f;
+                    bool dynamic = dynamics[i];
+                    Vector3 attribute = attributes[i];
+                    float mass = attribute.X;
+                    float friction = attribute.Y;
+                    float restitution = attribute.Z;
+
+
+                    // Build transformation matrix
+                    Matrix temp_matrix = EngineHelper.otk2bullet(
+                        EngineHelper.blender2Kailash(
+                            EngineHelper.createMatrix(
+                                EngineHelper.bullet2otk(position), 
+                                EngineHelper.bullet2otk(rotation), 
+                                new OpenTK.Vector3(1.0f)
+                    )));
+
+
+                    CollisionShape shape;
+                    string collision_shape = shapes[i];
+                    float radius;
+                    switch (collision_shape)
+                    {
+                        case "BOX":
+                            shape = new BoxShape(dimension);
+                            break;
+                        case "SPHERE":
+                            radius = (dimension.X + dimension.Y + dimension.Z) / 3.0f;
+                            shape = new SphereShape(radius);
+                            break;
+                        case "CONE":
+                            radius = (dimension.X + dimension.Z) / 4.0f;
+                            shape = new ConeShape(radius, dimension.Y);
+                            break;
+                        case "CYLINDER":
+                            shape = new CylinderShape(dimension);
+                            break;
+                        case "CONVEX_HULL":
+                            shape = loadConvexHull(temp_unique_mesh.mesh, scale);
+                            break;
+                        case "MESH":
+                            shape = loadConcaveMesh(temp_unique_mesh.mesh, scale);
+                            break;
+                        default:
+                            //numBodies--;
+                            Debug.DebugHelper.logError("\t" + collision_shape + " is not a supported collision shape", mesh_id);
+                            continue;
+                    }
+
+
+                    RigidBody collision_object = PhysicsHelper.CreateLocalRigidBody(physics_world, dynamic, mass, restitution, temp_matrix, shape, dimension);
+                    collision_object.UserObject = physics_id;
+
+
+
                     RigidBodyObject rigid_body_object = new RigidBodyObject(physics_id, collision_object, scale);
                     temp_unique_mesh.physics_object = rigid_body_object;
                 }
