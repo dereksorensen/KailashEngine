8e71b7eea86510a10ecf405fdaf9225520335118
goulash32 <mueller.hunter@gmail.com>
2016-11-06 17:17:51 -0800

    Renamed ScriptBase and ScriptEnvironment to LuaScriptBase and LuaScriptEnvironment. Made LuaScriptBase non-static, updated example Added SetGlobal functionality and example Updated LuaScriptEnvironment error handling to log errors and LuaScriptBase will now bubble up exceptions.

--- KailashEngine/Launcher.cs
+++ KailashEngine/Launcher.cs
@@ -47,67 +47,99 @@ namespace KailashEngine
             //*************************************************************************************
             //** Scripting Tests begin Here 
             //*************************************************************************************
-            
-            //Initialize ScriptBase (static class) and add initial test environment ("testbed")
-            //TODO: Make ScriptBase non-static so multiple instances can be created for multithreading
-            ScriptBase.Initialize();
-            ScriptBase.CreateEnvironment("testbed");
 
-            //Add a script named "test" with the Lua source "return a + b", which accepts two arguments (int a, int b)
-            ScriptBase.AddScript("testbed", "test", "return a + b", true, 
-                                new KeyValuePair<string, Type>("a", typeof(int)), 
-                                new KeyValuePair<string, Type>("b", typeof(int)));
-
-            //Run the "test" script and fetch the int return value
-            var result = ScriptBase.RunScript<int>("testbed", "test", 10, 20);
-            Console.WriteLine("Script test: 10 + 20 = " + result);
-
-            //Reverse scenario: Lua calling C# code
-            //Create a Func object out of a lambda, neat and clean.
-            Func<double> GetPi = () =>
+            try
             {
-                return Math.PI;
-            };
+                //Initialize ScriptBase (static class) and add initial test environment ("testbed")
+                //TODO: Make ScriptBase non-static so multiple instances can be created for multithreading
+                var scriptBase = new LuaScriptBase();
+                scriptBase.CreateEnvironment("testbed");
 
-            //Add our C# function cSharpFunc to environment "testbed"; 
-            //can now be used by any script in that environment
-            ScriptBase.AddFunction("testbed", "getPi", GetPi); 
-            
-            //Add another script and call it
-            ScriptBase.AddScript("testbed", "funcTest", "return getPi()", true);
-            var result2 = ScriptBase.RunScript<double>("testbed", "funcTest");
+                //Add a script named "test" with the Lua source "return a + b", which accepts two arguments (int a, int b).
+                //The fourth argument is true because the third argument is Lua source code rather than a filename.
+                scriptBase.AddScript("testbed", "test", "return a + b", true,
+                                    new KeyValuePair<string, Type>("a", typeof(int)),
+                                    new KeyValuePair<string, Type>("b", typeof(int)));
 
-            Console.WriteLine("Pi, according to Lua by way of C# = " + result2);
+                //Run the "test" script and fetch the int return value
+                var result = scriptBase.RunScript<int>("testbed", "test", 10, 20);
+                Console.WriteLine("Script test: 10 + 20 = " + result);
 
-            //Add a script containing a very simple Lua function to be called from C#
-            //The possibilities here are great: You could write a script 
-            //with functions like "onUpdate()", "onDestroy()", "onCreate()", etc. (which
-            //in turn call native C# functions), and call those in C#
-            //at the appropriate time; letting you drive your game logic through Lua
-            ScriptBase.AddScript("testbed", "getFunc", "function luaSays(text) \n"    +
-                                                             //You get full access to the CLR through the clr namespace in Lua. So cool.
-                                                        "    clr.System.Console.WriteLine('From Lua: ' .. text) \n" + // '..' is the Lua string concatenation operator; weird, I know!
-                                                        "    return text \n"   + 
-                                                        "end", 
-                                                        true);
+                //Reverse scenario: Lua calling C# code
+                //Create a Func object out of a lambda, neat and clean.
+                Func<double> GetPi = () =>
+                {
+                    return Math.PI;
+                };
 
-            //Must run the script initially so Lua function winds up in the environment.
-            //TODO: Make this step transparent (i.e. have the option for the system to run
-            //      the script automatically upon adding to establish the Lua functions it
-            //      contains; effectively a "compilation" step).
-            ScriptBase.RunScript("testbed", "getFunc");
+                //Add our C# function cSharpFunc to environment "testbed"; 
+                //can now be used by any script in that environment
+                scriptBase.AddFunction("testbed", "getPi", GetPi);
 
-            //The global space (environment) now contains the "luaSays(text)" Lua function,
-            //so we can assign it to a C# function object and call it at will.
-            var printline = ScriptBase.GetFunction<string>("testbed", "luaSays");
-            var result3 = printline("hey!");
+                //Add another script and call it
+                scriptBase.AddScript("testbed", "funcTest", "return getPi()", true);
+                var result2 = scriptBase.RunScript<double>("testbed", "funcTest");
 
-            Console.WriteLine("From C#: " + result3.ToType(typeof(string)));
+                Console.WriteLine("Pi, according to Lua by way of C# = " + result2);
 
-            //There're also helper methods to call Lua functions directly and extract the result
-            //Generic syntax is <Return Type, Argument 1 Type, Argument 2 Type, etc.> (up to four arguments).
-            string result4 = ScriptBase.CallFunction<string, string>("testbed", "luaSays", "You know it!");
-            Console.WriteLine("From C#: " + result4);
+                //Add a script containing a very simple Lua function to be called from C#
+                //The possibilities here are great: You could write a script 
+                //with functions like "onUpdate()", "onDestroy()", "onCreate()", etc. (which
+                //in turn call native C# functions), and call those in C#
+                //at the appropriate time; letting you drive your game logic through Lua
+                scriptBase.AddScript("testbed", "getFunc", "function luaSays(text) \n" +
+                                                            //You get full access to the CLR through the clr namespace in Lua. So cool.
+                                                            "    clr.System.Console.WriteLine('From Lua: ' .. text) \n" + // '..' is the Lua string concatenation operator; weird, I know!
+                                                            "    return text \n" +
+                                                            "end",
+                                                            true);
+
+                //Must run the script initially so Lua function winds up in the environment.
+                //TODO: Make this step transparent (i.e. have the option for the system to run
+                //      the script automatically upon adding to establish the Lua functions it
+                //      contains; effectively a "compilation" step).
+                scriptBase.RunScript("testbed", "getFunc");
+
+                //The global space (environment) now contains the "luaSays(text)" Lua function,
+                //so we can assign it to a C# function object and call it at will.
+                var printline = scriptBase.GetFunction<string>("testbed", "luaSays");
+                var result3 = printline("hey!");
+
+                Console.WriteLine("From C#: " + result3.ToType(typeof(string)));
+
+                //There're also helper methods to call Lua functions directly and extract the result
+                //Generic syntax is <Return Type, Argument 1 Type, Argument 2 Type, etc.> (up to four arguments).
+                string result4 = scriptBase.CallFunction<string, string>("testbed", "luaSays", "You know it!");
+                Console.WriteLine("From C#: " + result4);
+
+                //Setting globals can be useful for things like passing an ID through for the script to operate on,
+                //or for providing global constants to the scripts.
+                scriptBase.SetGlobal("testbed", "GLOBAL_ID_STRING", "1234567890ABCDEF");
+                scriptBase.SetGlobal("testbed", "GOOBLE_GOBBLE", "one of us one of us");
+                scriptBase.SetGlobal("testbed", "BOOBS", 80085);
+                scriptBase.SetGlobal("testbed", "this", 0xFF903909);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    //Programatically set globals double0 through double9. This is a fairly useless example, but the
+                    //possibilities are cool.        
+                    scriptBase.SetGlobal("testbed", "double" + i, i * 2);
+                }
+
+                //Again, luaSays(text) is now in the global environment, so we can use it again here without redefining.
+                //AddScript effectively functions like an "include" or "using" system. You could have a factory that
+                //returns collections of scripts (building a module system and default set of libraries that call engine 
+                //functions -- or simplified wrappers around engine functions -- in a contextually appropriate way would be a natural
+                //extension of this).
+                scriptBase.AddScript("testbed", "globals", "luaSays(GLOBAL_ID_STRING) \n luaSays(GOOBLE_GOBBLE) \n luaSays(tostring(BOOBS)) \n" +
+                                                           "luaSays(tostring(double9)) \n luaSays(tostring(double5)) \n luaSays(tostring(this)) \n",
+                                                           true);
+                scriptBase.RunScript("testbed", "globals");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             //*************************************************************************************
             //** Scripting Tests end here, set a breakpoint at next statement 
---
+++ KailashEngine/Scripting/LuaScriptBase.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using Neo.IronLua;
+using KailashEngine.Debug;
+
+namespace KailashEngine.Scripting
+{
+    public class LuaScriptBase
+    {
+        private Lua _context;
+        private Dictionary<string, LuaScriptEnvironment> _environments;
+
+        public LuaScriptBase()
+        {
+            try
+            {
+                _context = new Lua(LuaIntegerType.Int32, LuaFloatType.Double);
+                _environments = new Dictionary<string, LuaScriptEnvironment>();
+            }
+            catch(Exception e)
+            {
+                DebugHelper.logError("LuaScriptBase()", e.Message);
+                throw new Exception("Error, unable to intialize ScriptBase: " + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Add a script environment (isolated) to collection
+        /// </summary>
+        /// <param name="name">Name of the environment to add.</param>
+        public void CreateEnvironment(string name)
+        {
+            try
+            {
+                _environments[name] = new LuaScriptEnvironment(ref _context);
+            }
+            catch(Exception e)
+            {
+                DebugHelper.logError("CreateEnvironment()", e.Message);
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Add a script to the given environment
+        /// </summary>
+        /// <param name="environment">Name of script environment.</param>
+        /// <param name="name">Name of script.</param>
+        /// <param name="fileOrSource">File if next parameter is false (default), or literal source if true.</param>
+        /// <param name="isSource">Preceeding argument is source code? Else, it's a filename.</param>
+        /// <param name="par">Optional list of parameters for chunk. You are required to pass any parameters defined here 
+        /// when running the script.</param>
+        public void AddScript(string environment, string name, string fileOrSource, 
+                                    bool isSource = false, params KeyValuePair<string, Type>[] par)
+        {
+            try
+            {
+                // Compile script text or file to a LuaChunk, add to provided LuaPortableGlobal environment
+                if (isSource)
+                {
+                    var chunk = _context.CompileChunk(fileOrSource, name, 
+                        new LuaCompileOptions() { DebugEngine = LuaStackTraceDebugger.Default }, par);
+                    _environments[environment].AddScript(name, chunk);
+                }
+                else
+                {
+                    var chunk = _context.CompileChunk(fileOrSource, 
+                        new LuaCompileOptions() { DebugEngine = LuaStackTraceDebugger.Default }, par);
+                    _environments[environment].AddScript(name, chunk);
+                }
+            }
+            catch(Exception e)
+            {
+                DebugHelper.logError("AddScript()", e.Message);
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Add a C# function to a Lua environment's global space
+        /// </summary>
+        /// <typeparam name="R">Return type of function.</typeparam>
+        /// <param name="environment">Name of script environment.</param>
+        /// <param name="name">Name of script.</param>
+        /// <param name="funcName">Name of the function callable within Lua.</param>
+        /// <param name="func">C# Function to add.</param>
+        public void AddFunction<R>(string environment, string funcName, Func<R> func)
+        {
+            try
+            {
+                _environments[environment].AddFunction(funcName, func);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void AddFunction<R, T>(string environment, string funcName, Func<T, R> func)
+        {
+            try
+            {
+                _environments[environment].AddFunction(funcName, func);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void AddFunction<R, T1, T2>(string environment, string funcName, Func<T1, T2, R> func)
+        {
+            try
+            {
+                _environments[environment].AddFunction(funcName, func);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void AddFunction<R, T1, T2, T3>(string environment, string funcName, Func<T1, T2, T3, R> func)
+        {
+            try
+            {
+                _environments[environment].AddFunction(funcName, func);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void AddFunction<R, T1, T2, T3, T4>(string environment, string funcName, Func<T1, T2, T3, T4, R> func)
+        {
+            try
+            {
+                _environments[environment].AddFunction(funcName, func);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Get a function by name from the Lua environment.
+        /// </summary>
+        /// <param name="environment">Environment to search.</param>
+        /// <param name="funcName">The name of the function (case sensitive).</param>
+        /// <returns></returns>
+        public Func<LuaResult> GetFunction(string environment, string funcName)
+        {
+            try
+            {
+                return _environments[environment].GetFunction(funcName);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Func<T, LuaResult> GetFunction<T>(string environment, string funcName)
+        {
+            try
+            {
+                return _environments[environment].GetFunction<T>(funcName);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Func<T1, T2, LuaResult> GetFunction<T1, T2>(string environment, string funcName)
+        {
+            try
+            {
+                return _environments[environment].GetFunction<T1, T2>(funcName);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Func<T1, T2, T3, LuaResult> GetFunction<T1, T2, T3>(string environment, string funcName)
+        {
+            try
+            {
+                return _environments[environment].GetFunction<T1, T2, T3>(funcName);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Func<T1, T2, T3, T4, LuaResult> GetFunction<T1, T2, T3, T4>(string environment, string funcName)
+        {
+            try
+            {
+                return _environments[environment].GetFunction<T1, T2, T3, T4>(funcName);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Helper method to call a Lua function and extract the result.
+        /// </summary>
+        /// <typeparam name="R">The return type of the function.</typeparam>
+        /// <param name="environment">Environment to search.</param>
+        /// <param name="funcName">The name of the function (case sensitive).</param>
+        /// <returns></returns>
+        public R CallFunction<R>(string environment, string funcName)
+        {
+            try
+            {
+                var f = GetFunction(environment, funcName);
+                var result = f();
+                return (R)result.ToType(typeof(R));
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public R CallFunction<R, T>(string environment, string funcName, T arg)
+        {
+            try
+            {
+                var f = GetFunction<T>(environment, funcName);
+                var result = f(arg);
+                return (R)result.ToType(typeof(R));
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public R CallFunction<R, T1, T2>(string environment, string funcName, T1 arg1, T2 arg2)
+        {
+            try
+            {
+                var f = GetFunction<T1, T2>(environment, funcName);
+                var result = f(arg1, arg2);
+                return (R)result.ToType(typeof(R));
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public R CallFunction<R, T1, T2, T3>(string environment, string funcName, T1 arg1, T2 arg2, T3 arg3)
+        {
+            try
+            {
+                var f = GetFunction<T1, T2, T3>(environment, funcName);
+                var result = f(arg1, arg2, arg3);
+                return (R)result.ToType(typeof(R));
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public R CallFunction<R, T1, T2, T3, T4>(string environment, string funcName, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            try
+            {
+                var f = GetFunction<T1, T2, T3, T4>(environment, funcName);
+                var result = f(arg1, arg2, arg3, arg4);
+                return (R)result.ToType(typeof(R));
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Run a script that doesn't return anything.
+        /// </summary>
+        /// <param name="environment"></param>
+        /// <param name="environment">Name of script environment.</param>
+        /// <param name="name">Name of script.</param>
+        /// <param name="args">Optional list of arguments for lua func.</param>
+        public void RunScript(string environment, string name, params object[] args)
+        {
+            try
+            {
+                _environments[environment].ExecuteScript(name, args);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Run a script that returns a single value.
+        /// </summary>
+        /// <typeparam name="T">Type to return.</typeparam>
+        /// <param name="environment">Name of script environment.</param>
+        /// <param name="name">Name of script.</param>
+        /// <param name="args">Optional list of arguments for lua func.</param>
+        /// <returns></returns>
+        public T RunScript<T>(string environment, string name, params object[] args)
+        {
+            try
+            {
+                T res = _environments[environment].ExecuteScript<T>(name, args);
+                return res;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void SetGlobal<T>(string environment, string globalName, T value)
+        {
+            try
+            {
+                _environments[environment].SetGlobal(globalName, value);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Reset(string environment)
+        {
+            try
+            {
+                _environments[environment].Reset(ref _context);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Dictionary<string, LuaScriptEnvironment> Environments
+        {
+            get { return _environments; }
+        }
+    }
+}
